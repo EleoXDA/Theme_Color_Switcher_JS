31d277608a9ba76d6413a8875d807d2fdbc528a3
Elbay Malik <eleo.666@gmail.com>
2023-05-18 11:56:21 +0200

    Save the theme to local storage
    ... to load when page is reloaded

--- theme_color_switcher.js
+++ theme_color_switcher.js
@@ -1,7 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var themeSelector = document.getElementById('theme-selector');
-    themeSelector.addEventListener('change', function() {
-        document.body.classList.remove('theme-default', 'theme-dark', 'theme-light');         // Remove the existing theme class
-        document.body.classList.add(this.value);         // Add the selected theme class
+    var themeButtons = document.querySelectorAll('.theme-button');
+    
+    themeButtons.forEach(function(button) {
+        button.addEventListener('click', function() {
+            document.body.classList.remove('theme-default', 'theme-dark', 'theme-light');             // Remove the existing theme class
+            document.body.classList.add(this.dataset.theme);             // Add the selected theme class
+            localStorage.setItem('selectedTheme', this.dataset.theme);             // Save the selected theme in local storage
+        });
     });
+
+    // Load the saved theme from local storage (if any)
+    var savedTheme = localStorage.getItem('selectedTheme');
+    if (savedTheme) {
+        document.body.classList.remove('theme-default', 'theme-dark', 'theme-light');         // Remove the existing theme class
+        document.body.classList.add(savedTheme);         // Add the saved theme class
+    }
 });
